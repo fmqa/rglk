847e34835a1698971c382623b9bf828b4d63611b
Kochab <kochab@users.noreply.github.com>
2020-11-03 21:17:04 +0100

    Use template method pattern for actors that depend on EntityOperations

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as ROT from 'rot-js';
 import { AssetLoader } from './assets';
-import { classes } from './ecs/actors';
+import { EntityOperations, Floor, HamsterTemplate, MoneyTemplate, Wall } from './ecs/actors';
 import { Engine } from './ecs/engine';
 import { Entity } from './ecs/entities';
 import { simple, SimpleOSD } from './ecs/rendering';
@@ -34,14 +34,26 @@ export class Game {
 
         const engine = new Engine;
 
-        const F = classes({
+        const operations: EntityOperations = {
             add(entity) { return engine.add(entity); },
             delete(entity) { return engine.delete(entity); },
             find(position) { return engine.spatial.grid.find(position); },
             movement: engine.spatial
-        });
-        
-        const player = new F.Hamster;
+        };
+
+        class Hamster extends HamsterTemplate {
+            get operations() {
+                return operations;
+            }
+        }
+
+        class Money extends MoneyTemplate {
+            get operations() {
+                return operations;
+            }
+        }
+
+        const player = new Hamster;
 
         player.position.x = 128;
         player.position.y = 128;
@@ -59,10 +71,10 @@ export class Game {
         digger.create((x, y, content) => {
             let entity: Entity;
             if (content) {
-                entity = new F.Wall(x, y);
+                entity = new Wall(x, y);
             } else {
                 const what = ROT.RNG.getWeightedValue({floor: 99, tp: 1});
-                entity = what === 'floor' ? new F.Floor(x, y) : new F.Money(x, y);
+                entity = what === 'floor' ? new Floor(x, y) : new Money(x, y);
             }
             engine.add(entity);
         });
@@ -79,12 +91,12 @@ export class Game {
         let tp = 0;
 
         engine.spatial.events.on('collided', (a: Entity, b: Entity) => {
-            if (a instanceof F.Hamster) {
-                if (b instanceof F.Money) {
+            if (a instanceof Hamster) {
+                if (b instanceof Money) {
                     b.consumed(2, () => delete osd.text);
                     const s = ROT.RNG.getItem(["*om nom nom*", "*chmonk*", "*chomp*"])!;
                     osd.text = `${s} [${++tp}]`;
-                } else if (b instanceof F.Wall) {
+                } else if (b instanceof Wall) {
                     a.shake();
                 }
             }
--- src/ecs/actors.ts
+++ src/ecs/actors.ts
@@ -6,94 +6,94 @@ import { EntityActionQueueMixin, EntityTimerMixin } from "./mixins";
 /**
  * Entity operations facade
  */
-export interface EntityOps {
+export interface EntityOperations {
     add(entity: Entity): void;
     delete(entity: Entity): void;
     find(position: EntityPosition): Entity | undefined;
     movement: MovementBuilder;
 }
 
-export function classes(ops: EntityOps) {
-    class Hamster extends EntityTimerMixin(EntityActionQueueMixin(BaseEntity)) {
-        image: EntityImage = {tile: "\u{1F439}", fg: "white", bg: "transparent"};
-        position = new EntityPosition(0, 0, 1);
-        movement?: EntityMovement;
-        speed = 4;
+export abstract class HamsterTemplate extends EntityTimerMixin(EntityActionQueueMixin(BaseEntity)) {
+    protected abstract operations: EntityOperations;
 
-        navigate(x: number, y: number) {
+    image: EntityImage = {tile: "\u{1F439}", fg: "white", bg: "transparent"};
+    position = new EntityPosition(0, 0, 1);
+    movement?: EntityMovement;
+    speed = 4;
+
+    navigate(x: number, y: number) {
+        this.queue.cancel();
+        const astar = new AStar(x, y, (x, y) => !this.operations.find(new EntityPosition(x, y, this.position.z))?.obstacle, {topology: 4});
+        const waypoints: Point[] = [];
+        astar.compute(Math.round(this.position.x), Math.round(this.position.y), (x, y) => waypoints.push([x, y]));
+        this.queue.push(signal => this.operations.movement.composite(this, this.speed, waypoints, signal));
+    }
+
+    shake() {
+        if (this.movement) {
+            const {x, y} = this.position;
+            const sx = Math.sign(this.movement.x - x);
+            const sy = Math.sign(this.movement.y - y);
             this.queue.cancel();
-            const astar = new AStar(x, y, (x, y) => !ops.find(new EntityPosition(x, y, this.position.z))?.obstacle, {topology: 4});
-            const waypoints: Point[] = [];
-            astar.compute(Math.round(this.position.x), Math.round(this.position.y), (x, y) => waypoints.push([x, y]));
-            this.queue.push(signal => ops.movement.composite(this, this.speed, waypoints, signal));
-        }
-
-        shake() {
-            if (this.movement) {
-                const {x, y} = this.position;
-                const sx = Math.sign(this.movement.x - x);
-                const sy = Math.sign(this.movement.y - y);
-                this.queue.cancel();
-                const token = this.timer.defer(async (signal) => {
-                    for (let i = 1; i <= 12; i++) {
-                        if (signal.aborted) {
-                            break;
-                        }
-                        this.position.x += -sx / 100;
-                        this.position.y += -sy / 100;
-                        await this.timer.sleep(0.01);
+            const token = this.timer.defer(async (signal) => {
+                for (let i = 1; i <= 12; i++) {
+                    if (signal.aborted) {
+                        break;
                     }
-                    this.position.x = x;
-                    this.position.y = y;
-                    this.queue.cancellation.delete(token);
-                });
-                this.queue.cancellation.add(token);
-            }
-        }
-    }
-
-    class Floor implements Entity {
-        image: EntityImage = {tile: " ", fg: "white"};
-        position: EntityPosition;
-
-        constructor(x: number, y: number) {
-            this.position = new EntityPosition(x, y, 0);
-        }
-    }
-
-    class Wall implements Entity {
-        image: EntityImage = {tile: "\u{1F9F1}", fg: "white"};
-        position: EntityPosition;
-        obstacle = true;
-
-        constructor(x: number, y: number) {
-            this.position = new EntityPosition(x, y, 1);
-        }
-    }
-
-    class Money extends EntityTimerMixin(BaseEntity) {
-        image: EntityImage = {tile: "\u{1F9FB}", fg: "white"};
-        position: EntityPosition;
-
-        constructor(x: number, y: number) {
-            super();
-            this.position = new EntityPosition(x, y, 1);
-        }
-
-        consumed(z: number, callback?: (entity: this) => void) {
-            // update z layer
-            ops.delete(this);
-            this.position.z = z;
-            ops.add(this);
-            // trigger animation
-            this.timer.defer(async () => {
-                this.image.tile = "\u{1F4A8}";
-                await this.timer.sleep(1);
-                ops.delete(this);
-                callback?.(this);
+                    this.position.x += -sx / 100;
+                    this.position.y += -sy / 100;
+                    await this.timer.sleep(0.01);
+                }
+                this.position.x = x;
+                this.position.y = y;
+                this.queue.cancellation.delete(token);
             });
+            this.queue.cancellation.add(token);
         }
     }
+}
 
-    return {Hamster, Floor, Wall, Money};
+export class Floor implements Entity {
+    image: EntityImage = {tile: " ", fg: "white"};
+    position: EntityPosition;
+
+    constructor(x: number, y: number) {
+        this.position = new EntityPosition(x, y, 0);
+    }
+}
+
+export class Wall implements Entity {
+    image: EntityImage = {tile: "\u{1F9F1}", fg: "white"};
+    position: EntityPosition;
+    obstacle = true;
+
+    constructor(x: number, y: number) {
+        this.position = new EntityPosition(x, y, 1);
+    }
+}
+
+export abstract class MoneyTemplate extends EntityTimerMixin(BaseEntity) {
+    protected abstract operations: EntityOperations;
+
+    image: EntityImage = {tile: "\u{1F9FB}", fg: "white"};
+    position: EntityPosition;
+
+    constructor(x: number, y: number) {
+        super();
+        this.position = new EntityPosition(x, y, 1);
+    }
+
+    consumed(z: number, callback?: (entity: this) => void) {
+        // update z layer
+        this.operations.delete(this);
+        this.position.z = z;
+        this.operations.add(this);
+        // trigger animation
+        this.timer.defer(async () => {
+            this.image.tile = "\u{1F4A8}";
+            await this.timer.sleep(1);
+            this.operations.delete(this);
+            callback?.(this);
+        });
+    }
 }
